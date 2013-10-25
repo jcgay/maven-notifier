d262562916fbde527cf9c855db038157d7527e29
Jean-Christophe GAY <contact@jeanchristophegay.com>
2013-10-25 22:08:22 +0200

    Add a subtitle indicating build status for OSX notifications.

--- src/main/java/com/github/jcgay/maven/notifier/notificationcenter/NotificationCenterNotifier.java
+++ src/main/java/com/github/jcgay/maven/notifier/notificationcenter/NotificationCenterNotifier.java
@@ -1,5 +1,10 @@
 package com.github.jcgay.maven.notifier.notificationcenter;
 
+import java.util.concurrent.TimeUnit;
+
+import org.apache.maven.execution.MavenExecutionResult;
+import org.codehaus.plexus.component.annotations.Component;
+
 import com.github.jcgay.maven.notifier.AbstractCustomEventSpy;
 import com.github.jcgay.maven.notifier.Configuration;
 import com.github.jcgay.maven.notifier.Notifier;
@@ -8,14 +13,13 @@ import com.github.jcgay.maven.notifier.executor.Executor;
 import com.github.jcgay.maven.notifier.executor.RuntimeExecutor;
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Joiner;
-import org.apache.maven.execution.MavenExecutionResult;
-import org.codehaus.plexus.component.annotations.Component;
 
 @Component(role = Notifier.class, hint = "notification-center")
 public class NotificationCenterNotifier extends AbstractCustomEventSpy {
 
     private static final String CMD_MESSAGE = "-message";
     private static final String CMD_TITLE = "-title";
+    private static final String CMD_SUBTITLE = "-subtitle";
     private static final String CMD_GROUP = "-group";
     private static final String CMD_OPEN = "-open";
     private static final String GROUP = "maven";
@@ -40,25 +44,34 @@ public class NotificationCenterNotifier extends AbstractCustomEventSpy {
 
     @Override
     protected String buildNotificationMessage(MavenExecutionResult result) {
-        StringBuilder builder = new StringBuilder();
-        builder.append(result.getProject().getName());
-        builder.append(": ");
-        Status status = result.hasExceptions() ? Status.FAILURE : Status.SUCCESS;
-        builder.append(status.message());
-        return builder.toString();
+        if (status(result) == Status.SUCCESS) {
+            long time = result.getBuildSummary(result.getProject()).getTime();
+            StringBuilder builder = new StringBuilder("Built in: ");
+            builder.append(TimeUnit.MILLISECONDS.toSeconds(time));
+            builder.append(" second(s).");
+            return builder.toString();
+        }
+
+        return "";
+    }
+
+    private Status status(MavenExecutionResult result) {
+        return result.hasExceptions() ? Status.FAILURE : Status.SUCCESS;
     }
 
     private String[] buildCommand(MavenExecutionResult result) {
-        String[] commands = new String[9];
+        String[] commands = new String[11];
         commands[0] = configuration.getNotificationCenterPath();
         commands[1] = CMD_TITLE;
         commands[2] = result.getProject().getName();
-        commands[3] = CMD_MESSAGE;
-        commands[4] = buildNotificationMessage(result);
-        commands[5] = CMD_GROUP;
-        commands[6] = GROUP;
-        commands[7] = CMD_OPEN;
-        commands[8] = result.getProject().getBuild().getDirectory();
+        commands[3] = CMD_SUBTITLE;
+        commands[4] = status(result).message();
+        commands[5] = CMD_MESSAGE;
+        commands[6] = buildNotificationMessage(result);
+        commands[7] = CMD_GROUP;
+        commands[8] = GROUP;
+        commands[9] = CMD_OPEN;
+        commands[10] = result.getProject().getBuild().getDirectory();
 
         if (logger.isDebugEnabled()) {
             logger.debug("Will execute command line: " + Joiner.on(" ").skipNulls().join(commands));
--- src/test/java/com/github/jcgay/maven/notifier/notificationcenter/NotificationCenterNotifierTest.java
+++ src/test/java/com/github/jcgay/maven/notifier/notificationcenter/NotificationCenterNotifierTest.java
@@ -1,20 +1,22 @@
 package com.github.jcgay.maven.notifier.notificationcenter;
 
-import com.github.jcgay.maven.notifier.Configuration;
-import com.github.jcgay.maven.notifier.executor.ExecutorHolder;
-import com.github.jcgay.maven.notifier.growl.GrowlNotifier;
-import org.apache.maven.execution.DefaultMavenExecutionResult;
-import org.apache.maven.model.Build;
-import org.apache.maven.project.MavenProject;
-import org.codehaus.plexus.logging.Logger;
-import org.mockito.Mockito;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
-
 import static junit.framework.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertTrue;
+
+import org.apache.maven.execution.BuildSuccess;
+import org.apache.maven.execution.DefaultMavenExecutionResult;
+import org.apache.maven.model.Build;
+import org.apache.maven.project.MavenProject;
+import org.codehaus.plexus.logging.Logger;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.github.jcgay.maven.notifier.Configuration;
+import com.github.jcgay.maven.notifier.Status;
+import com.github.jcgay.maven.notifier.executor.ExecutorHolder;
+import com.github.jcgay.maven.notifier.growl.GrowlNotifier;
 
 public class NotificationCenterNotifierTest {
 
@@ -41,18 +43,21 @@ public class NotificationCenterNotifierTest {
         build.setDirectory("/Applications");
         project.setBuild(build);
         event.setProject(project);
+        event.addBuildSummary(new BuildSuccess(project, 1000));
 
         notifier.onEvent(event);
 
         assertEquals("path", result.getCommand()[0]);
         assertEquals("-title", result.getCommand()[1]);
         assertEquals(project.getName(), result.getCommand()[2]);
-        assertEquals("-message", result.getCommand()[3]);
-        assertEquals("project: Success", result.getCommand()[4]);
-        assertEquals("-group", result.getCommand()[5]);
-        assertEquals("maven", result.getCommand()[6]);
-        assertEquals("-open", result.getCommand()[7]);
-        assertEquals("/Applications", result.getCommand()[8]);
+        assertEquals("-subtitle", result.getCommand()[3]);
+        assertEquals(Status.SUCCESS.message(), result.getCommand()[4]);
+        assertEquals("-message", result.getCommand()[5]);
+        assertEquals("Built in: 1 second(s).", result.getCommand()[6]);
+        assertEquals("-group", result.getCommand()[7]);
+        assertEquals("maven", result.getCommand()[8]);
+        assertEquals("-open", result.getCommand()[9]);
+        assertEquals("/Applications", result.getCommand()[10]);
     }
 
     @Test
