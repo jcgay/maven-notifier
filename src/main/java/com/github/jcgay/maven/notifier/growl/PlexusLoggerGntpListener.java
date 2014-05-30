77ba7213077d2dc7270c2ead7ad9924476e09474
Jean-Christophe GAY <contact@jeanchristophegay.com>
2014-05-30 20:24:16 +0200

    Add wiki links when Growl notification fails [Fixes #6]
    When a error is logged, a Maven style message with wiki links
    is created.

--- src/main/java/com/github/jcgay/maven/notifier/growl/PlexusLoggerGntpListener.java
+++ src/main/java/com/github/jcgay/maven/notifier/growl/PlexusLoggerGntpListener.java
@@ -7,6 +7,9 @@ import org.codehaus.plexus.logging.Logger;
 
 public class PlexusLoggerGntpListener implements GntpListener {
 
+    private static final String OSX_URL = "https://github.com/jcgay/maven-notifier/wiki/Growl-(OS-X)";
+    private static final String WINDOWS_URL = "https://github.com/jcgay/maven-notifier/wiki/Growl-(Windows)";
+
     private final Logger logger;
 
     public PlexusLoggerGntpListener(Logger logger) {
@@ -40,16 +43,33 @@ public class PlexusLoggerGntpListener implements GntpListener {
 
     @Override
     public void onRegistrationError(GntpErrorStatus gntpErrorStatus, String s) {
-        logger.error(String.format("Growl Registration error: %s - %s", gntpErrorStatus, s));
+        logger.error(message(String.format("Growl Registration error: %s - %s", gntpErrorStatus, s)));
     }
 
     @Override
     public void onNotificationError(GntpNotification gntpNotification, GntpErrorStatus gntpErrorStatus, String s) {
-        logger.error(String.format("Growl Notification error: %s - %s - %s", gntpErrorStatus, s, gntpNotification.getText()));
+        logger.error(message(String.format("Growl Notification error: %s - %s - %s", gntpErrorStatus, s, gntpNotification.getText())));
     }
 
     @Override
     public void onCommunicationError(Throwable throwable) {
-        logger.error("Growl Communication error", throwable);
+        logger.error(message("Growl Communication error"), throwable);
+    }
+
+    private static String message(String message) {
+        return String.format("%s%n%n" +
+                        "For more information about the errors and possible solutions, please read the following article:%n%s",
+                message, getUrl());
+    }
+
+    private static String getUrl() {
+        String os = System.getProperty("os.name").toLowerCase();
+        if (os.contains("win")) {
+            return WINDOWS_URL;
+        }
+        if (os.contains("mac")) {
+            return OSX_URL;
+        }
+        return "https://github.com/jcgay/maven-notifier/wiki";
     }
 }
